acdc276611b212788ce4ed8417aa91e1572cd469
Unknown <ajsivesind@gmail.com>
2020-03-02 03:18:11 +0100

    feat: added basic exception handling

--- computenode/computenode.py
+++ computenode/computenode.py
@@ -1,5 +1,20 @@
+import atexit
+import logging
+import traceback
+
 from lifecycle_handler import Lifecycle
 
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
 lifecycle = Lifecycle("all")
-lifecycle.start("all")
-lifecycle.wait_for_exit()
+atexit.register(lifecycle.exit)
+
+try:
+    lifecycle.start("all")
+    lifecycle.wait_for_exit()
+except Exception as e:
+    logger.error("Encountered error:")
+    logger.error(traceback.format_exc())
+    logger.info("Stopping remaining services...")
+    lifecycle.exit()
